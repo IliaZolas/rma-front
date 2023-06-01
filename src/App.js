801144061e1ce0295dfde1a9c585c48a9a356d62
Ilia <52299150+IliaZolas@users.noreply.github.com>
2023-06-01 10:45:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Home from './pages/home';
 import Navbar from './components/navbar';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
-
 function App() {
   return (
     <div className="App">
